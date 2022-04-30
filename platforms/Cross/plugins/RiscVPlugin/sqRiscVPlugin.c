8ced9fa3ec40d23cb77abd8b32f4d8cedcba06c2
Leon Matthes <leon@matthes.biz>
2022-04-30 21:03:26 +0200

    Start work on sqRiscVPlugin.c

--- platforms/Cross/plugins/RiscVPlugin/sqRiscVPlugin.c
+++ platforms/Cross/plugins/RiscVPlugin/sqRiscVPlugin.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdbool.h>
 
 #define COG 1
 #define FOR_COG_PLUGIN 1
@@ -6,36 +7,57 @@
 #include "sqSetjmpShim.h"
 #include "RiscVPlugin.h"
 
-#include "riscv_example_soc.h"
+#include "core.h"
 
 void (*prevInterruptCheckChain)() = 0;
 long resetCPU(void *cpu);
 
+typedef struct squeak_soc {
+    rv_core_td core;
+    bool is_initialized;
+} squeak_soc_t;
+
+static squeak_soc_t soc;
+
 void *newCPU() {
-	return (void*) 42;
+    if (!soc.is_initialized) {
+        resetCPU(&soc);
+        soc.is_initialized = true;
+    }
+    return &soc;
+}
+
+static rv_ret squeak_soc_bus_access(void* privat, privilege_level priv_level, bus_access_type access_type, rv_uint_xlen address, void* value, uint8_t len)
+{
+    squeak_soc_t *soc = privat;
+    
+    // TOOD
+    return rv_err;
 }
 
 long resetCPU(void *cpu) {
-	rv_soc_td soc;
-	rv_soc_init(&soc, "test.fw", "test.db");
-	return 43;
+    squeak_soc_t *soc = cpu;
+
+    rv_core_init(&soc->core, soc, squeak_soc_bus_access);
+
+    return 0;
 }
 
 static inline void resetSegmentRegisters(uintptr_t byteSize,
-                                         uintptr_t minWriteMaxExecAddr) {
+        uintptr_t minWriteMaxExecAddr) {
 }
 
 long singleStepCPUInSizeMinAddressReadWrite(void *cpu, void *memory,
-                                            uintptr_t byteSize,
-                                            uintptr_t minAddr,
-                                            uintptr_t minWriteMaxExecAddr) {
-	return 44;
+        uintptr_t byteSize,
+        uintptr_t minAddr,
+        uintptr_t minWriteMaxExecAddr) {
+    return 44;
 }
 
 long runCPUInSizeMinAddressReadWrite(void *cpu, void *memory,
-                                     uintptr_t byteSize, uintptr_t minAddr,
-                                     uintptr_t minWriteMaxExecAddr) {
-	return 45;
+        uintptr_t byteSize, uintptr_t minAddr,
+        uintptr_t minWriteMaxExecAddr) {
+    return 45;
 }
 
 /*
@@ -45,8 +67,8 @@ void flushICacheFromTo(void *cpu, uintptr_t saddr, uintptr_t eaddr) {
 }
 
 long disassembleForAtInSize(void *cpu, uintptr_t laddr, void *memory,
-                            uintptr_t byteSize) {
-	return 46;
+        uintptr_t byteSize) {
+    return 46;
 }
 
 void forceStopRunning(void) {
@@ -55,7 +77,7 @@ void forceStopRunning(void) {
 long errorAcorn(void) { return 47; }
 
 char *getlog(long *len) {
-	return "Hello World";
+    return "Hello World";
 }
 void storeIntegerRegisterStateOfinto(void *cpu, WordType *registerState) {
 
--- platforms/unix/plugins/RiscVPlugin/acinclude.m4
+++ platforms/unix/plugins/RiscVPlugin/acinclude.m4
@@ -1,4 +1,4 @@
-# To build the RiscVPlugin one must build the Bochs support libraries
+# To build the RiscVPlugin one must build the riscv_em library first
 if test -f ../../../../processors/riscv_em/build/libriscv_em.a; then
 	echo "risv_em library has been built; building RiscVPlugin"
 else
